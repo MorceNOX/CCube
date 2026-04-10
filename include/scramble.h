cf1b72c579a851297706b83d499acfd24544bddb
MorceNOX <amilcar.rizk@gmail.com>
2026-04-09 23:29:16 -0300

    Add files via upload
    Update the number of scrambles to 25

--- include/scramble.h
+++ include/scramble.h
@@ -1,6 +1,8 @@
 #ifndef SCRAMBLE_H
 #define SCRAMBLE_H
 
+#define N_SCRAMBLES 25
+
 void get_scramble(const char *full_scramble, char *scramble);
 void generate_scramble(char *scramble);
 void split_scramble(const char *full_scramble, char *scrambleA, char *scrambleB, char *scrambleC);
